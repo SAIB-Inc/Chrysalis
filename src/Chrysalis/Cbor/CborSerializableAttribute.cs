f00e491703e8c4d9536042d00463c88721d4c953
windss <tamayowendell05@gmail.com>
2024-08-31 01:03:14 +0800

    chore: add indefinite field for `CborSerializableAttribute`

--- src/Chrysalis/Cbor/CborSerializableAttribute.cs
+++ src/Chrysalis/Cbor/CborSerializableAttribute.cs
@@ -6,9 +6,21 @@ public class CborSerializableAttribute(CborType type) : Attribute
 {
     public CborType Type { get; } = type;
     public int Index { get; set; } = -1;
+    public bool IsIndefinite { get; set; } = false;
 
     public CborSerializableAttribute(CborType type, int index) : this(type)
     {
         Index = index;
+    }
+
+    public CborSerializableAttribute(CborType type, bool isIndefinite) : this(type)
+    {
+        IsIndefinite = isIndefinite;
+    }
+
+    public CborSerializableAttribute(CborType type, int index, bool isIndefinite) : this(type)
+    {
+        Index = index;
+        IsIndefinite = isIndefinite;
     }
 }
