f8b6d8ad7fd42e4ef36e0d63af07288bd0247f53
windss <tamayowendell05@gmail.com>
2024-08-31 01:04:59 +0800

    feat: add util methods for getting `Value` and getting `CborType`

--- src/Chrysalis/Utils/CborSerializerUtils.cs
+++ src/Chrysalis/Utils/CborSerializerUtils.cs
@@ -1,8 +1,10 @@
 using System.Formats.Cbor;
+using System.Reflection;
+using Chrysalis.Cbor;
 
 namespace Chrysalis.Utils;
 
-public class CborSerializerUtils
+public static class CborSerializerUtils
 {
     private const int BaseTagValue = 121;
 
@@ -10,5 +12,33 @@ public class CborSerializerUtils
     {
         int actualIndex = index ?? 0;
         return (CborTag)(BaseTagValue + actualIndex);
+    }
+
+    public static object GetValue(this ICbor cbor, Type objType)
+    {
+        if (cbor == null)
+            throw new ArgumentNullException(nameof(cbor), "The CBOR object cannot be null.");
+
+        if (objType == null)
+            throw new ArgumentNullException(nameof(objType), "The target type cannot be null.");
+
+        PropertyInfo? valueProperty = objType.GetProperty("Value") 
+            ?? throw new InvalidOperationException($"Type {objType.Name} does not have a 'Value' property.");
+
+        object? value = valueProperty.GetValue(cbor) 
+            ?? throw new InvalidOperationException($"The 'Value' property of type {objType.Name} is null.");
+
+        return value;
+    }
+
+    public static CborType? GetCborType(this Type objType)
+    {
+        if (typeof(ICbor).IsAssignableFrom(objType))
+        {
+            CborSerializableAttribute? attr = objType.GetCustomAttribute<CborSerializableAttribute>();
+            if (attr != null)
+                return attr.Type;
+        }
+        return null;
     }
 }
