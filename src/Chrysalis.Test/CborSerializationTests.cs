763a85d82018dfaf42b85fc993af1c56d7fe40a0
windss <tamayowendell05@gmail.com>
2024-08-31 01:05:54 +0800

    feat: add test for `Treasury`, `PosixTime` and `MultisigScript`

--- src/Chrysalis.Test/CborSerializationTests.cs
+++ src/Chrysalis.Test/CborSerializationTests.cs
@@ -1,42 +1,44 @@
-using System.Formats.Cbor;
 using System.Reflection;
 using Chrysalis.Cardano.Models;
+using Chrysalis.Cardano.Models.Coinecta;
 using Chrysalis.Cbor;
 using Xunit;
 
-namespace Chrysalis.Test
+namespace Chrysalis.Test;
+
+public class CborSerializerTests
 {
-    public class CborSerializerTests
+    [Theory]
+    [InlineData("1834", typeof(CborInt))] // Example hex for CBOR int 52
+    [InlineData("4101", typeof(CborBytes))] // Example hex for CBOR bytes {0x01)]
+    [InlineData("1a000f4240", typeof(CborUlong))] // Example hex for CBOR ulong 1_000_000
+    [InlineData("1a000f4240", typeof(PosixTime))] // Example hex for CBOR ulong 1_000_000
+    [InlineData("43414243", typeof(CborBytes))] // Example hex for CBOR bytes of `ABC` string
+    [InlineData("a141614541696b656e", typeof(CborMap<CborBytes, CborBytes>))]
+    [InlineData("9f0102030405ff", typeof(CborList<CborInt>))] // [_ 1, 2, 3, 4, 5]
+    [InlineData("d8799f182aff", typeof(Option<CborInt>))] // Serialized CBOR for Option::Some(42):
+    [InlineData("d87a80", typeof(Option<CborInt>))] // Serialized CBOR for Option::None:
+    [InlineData("d8799f4180ff", typeof(Signature))] // Serialized CBOR for Signature:
+    [InlineData("d8799fd8799f581ceca3dfbde8ccb8408cefacda690e34aa9353af93fc02e75d8ba42f1bff58202325f3c999b17d4a6399bf6c02e1ff7615c13a73ecafae7fe813b9757f27ef2600ff", typeof(Treasury))] // Serialized CBOR for Signature:
+    public void SerializeAndDeserializePrimitives(string cborHex, Type type)
     {
-        [Theory]
-        [InlineData("1834", typeof(CborInt))] // Example hex for CBOR int 52
-        [InlineData("4101", typeof(CborBytes))] // Example hex for CBOR bytes {0x01)]
-        [InlineData("1a000f4240", typeof(CborUlong))] // Example hex for CBOR ulong 1_000_000
-        [InlineData("43414243", typeof(CborBytes))] // Example hex for CBOR bytes of `ABC` string
-        [InlineData("a141614541696b656e", typeof(CborMap<CborBytes, CborBytes>))]
-        [InlineData("9f0102030405ff", typeof(CborList<CborInt>))] // [_ 1, 2, 3, 4, 5]
-        [InlineData("d8799f182aff", typeof(Option<CborInt>))] // Serialized CBOR for Option::Some(42):
-        [InlineData("d87a80", typeof(Option<CborInt>))] // Serialized CBOR for Option::None:
-        public void SerializeAndDeserializePrimitives(string cborHex, Type type)
-        {
-            // Arrange
-            byte[] cborBytes = Convert.FromHexString(cborHex);
+        // Arrange
+        byte[] cborBytes = Convert.FromHexString(cborHex);
 
-            // Act
-            MethodInfo? deserializeMethod = typeof(CborSerializer).GetMethod(nameof(CborSerializer.Deserialize));
-            Assert.NotNull(deserializeMethod);
+        // Act
+        MethodInfo? deserializeMethod = typeof(CborSerializer).GetMethod(nameof(CborSerializer.Deserialize));
+        Assert.NotNull(deserializeMethod);
 
-            MethodInfo? genericDeserializeMethod = deserializeMethod?.MakeGenericMethod(type);
-            Assert.NotNull(genericDeserializeMethod);
+        MethodInfo? genericDeserializeMethod = deserializeMethod?.MakeGenericMethod(type);
+        Assert.NotNull(genericDeserializeMethod);
 
-            object? cborObject = genericDeserializeMethod?.Invoke(null, [cborBytes, null]);
-            Assert.NotNull(cborObject);
-            
-            var serializedBytes = CborSerializer.Serialize((ICbor)cborObject);
-            string serializedHex = Convert.ToHexString(serializedBytes).ToLowerInvariant();
+        object? cborObject = genericDeserializeMethod?.Invoke(null, [cborBytes]);
+        Assert.NotNull(cborObject);
 
-            // Assert
-            Assert.Equal(cborHex, serializedHex);
-        }
+        byte[] serializedBytes = CborSerializer.Serialize((ICbor)cborObject);
+        string serializedHex = Convert.ToHexString(serializedBytes).ToLowerInvariant();
+
+        // Assert
+        Assert.Equal(cborHex, serializedHex);
     }
 }
