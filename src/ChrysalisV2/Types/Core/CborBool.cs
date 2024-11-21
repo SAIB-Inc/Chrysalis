41ecd93a7205b565e1a5fc176c3937326f7ede0f
rjlacanlaled <rjlacanlaled@gmail.com>
2024-11-20 23:35:00 -0500

    remove constructor and default value

--- src/ChrysalisV2/Types/Core/CborBool.cs
+++ src/ChrysalisV2/Types/Core/CborBool.cs
@@ -3,7 +3,4 @@ using ChrysalisV2.Attributes;
 namespace ChrysalisV2.Types.Core;
 
 [CborSerializable(typeof(CborBool))]
-public record CborBool(bool Value = false) : RawCbor
-{
-    public CborBool() : this(false) { }
-}
+public record CborBool(bool Value) : RawCbor;
