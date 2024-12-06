8ad5212ee9785fd0c5ca50b3743ba7ff952917a5
Kathea Mari Mayol <114815547+Katteu@users.noreply.github.com>
2024-12-07 04:53:52 +0800

    refactor: add NullableConverter and CborNullable type with associated test data (#57)

---
+++ src/Chrysalis/Cbor/Converters/Primitives/NullableConverter.cs
@@ -0,0 +1,57 @@
+using System.Formats.Cbor;
+using Chrysalis.Cbor.Types;
+using System.Reflection;
+
+namespace Chrysalis.Cbor.Converters.Primitives;
+
+public class NullableConverter : ICborConverter
+{
+    public T Deserialize<T>(byte[] data) where T : CborBase
+    {
+        CborReader reader = new(data);
+        Type targetType = typeof(T);
+        Type valueType = targetType.GetGenericArguments()[0];
+
+        ConstructorInfo constructor = targetType.GetConstructor([valueType])
+            ?? throw new InvalidOperationException($"Type {targetType.Name} does not have a suitable constructor.");
+
+        object? value;
+        if (reader.PeekState() == CborReaderState.Null)
+        {
+            reader.ReadNull();
+            value = null;
+        }
+        else
+        {
+            value = typeof(CborSerializer)
+                .GetMethod(nameof(CborSerializer.Deserialize))!
+                .MakeGenericMethod(valueType)
+                .Invoke(null, [data]);
+        }
+
+        CborBase instance = (T)constructor.Invoke([value]);
+        instance.Raw = data;
+        return (T)constructor.Invoke([value]);
+    }
+
+    public byte[] Serialize(CborBase data)
+    {
+        Type valueType = data.GetType().GetGenericArguments()[0];
+        PropertyInfo? valueProperty = data.GetType().GetProperties()
+            .FirstOrDefault(p => p.PropertyType == valueType && p.Name != nameof(CborBase.Raw))
+            ?? throw new InvalidOperationException($"No property of type {valueType.Name} found in Cbor object.");
+
+        object? value = valueProperty.GetValue(data);
+
+        if (value is null)
+        {
+            CborWriter writer = new();
+            writer.WriteNull();
+            return writer.Encode();
+        }
+        else
+        {
+            return CborSerializer.Serialize((CborBase)value);
+        }
+    }
+}
