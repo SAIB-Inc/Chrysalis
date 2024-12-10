6af132f4d64c78ee81a33c265689be2ad138c6ee
Katteu <katheamari@gmail.com>
2024-12-11 04:21:52 +0800

    feat: add JpegAcceptOfferRedeemer type with CBOR serialization support

---
+++ src/Chrysalis/Cardano/Jpg/Types/Redeemers/AcceptOfferRedeemer.cs
@@ -0,0 +1,9 @@
+using Chrysalis.Cbor.Attributes;
+using Chrysalis.Cbor.Converters.Primitives;
+using Chrysalis.Cbor.Types;
+
+namespace Chrysalis.Cardano.Jpg.Types.Redeemers;
+
+[CborConverter(typeof(ConstrConverter))]
+[CborIndex(1)]
+public record JpegAcceptOfferRedeemer() : CborBase;
