f1bae4299f7094690a830b2b387025de12158ee3
Rj Lacanlale <rjlacanlaled@gmail.com>
2024-12-10 13:18:49 -0500

    refactor: update ci/cd and csproj config (#69)

--- src/Chrysalis.Test/CborTests.cs
+++ src/Chrysalis.Test/CborTests.cs
@@ -76,7 +76,7 @@ public class CborTests
         Block block = null;
         try
         {
-            for (int i = 0; i < 1000; i++)
+            for (int i = 0; i < 1; i++)
             {
                 block = CborSerializer.Deserialize<Block>(cborRaw);
             }
