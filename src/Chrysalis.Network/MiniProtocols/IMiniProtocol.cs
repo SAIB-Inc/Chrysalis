683c18e753fa03f4bb21b486c8f63e6c020292e4
rjlacanlaled <rjlacanlaled@gmail.com>
2025-03-07 13:07:35 -0500

    refactor: define the mini protocol interface

--- src/Chrysalis.Network/MiniProtocols/IMiniProtocol.cs
+++ src/Chrysalis.Network/MiniProtocols/IMiniProtocol.cs
@@ -1,3 +1,20 @@
+using Chrysalis.Network.Core;
+using Chrysalis.Network.Multiplexer;
+
 namespace Chrysalis.Network.MiniProtocols;
 
-public interface IMiniProtocol {}
+/// <summary>
+/// Defines the interface for Ouroboros mini-protocols.
+/// </summary>
+public interface IMiniProtocol
+{
+    /// <summary>
+    /// Gets the underlying communication channel for the protocol.
+    /// </summary>
+    AgentChannel Channel { get; }
+
+    /// <summary>
+    /// Gets the protocol type identifier.
+    /// </summary>
+    ProtocolType ProtocolType { get; }
+}
