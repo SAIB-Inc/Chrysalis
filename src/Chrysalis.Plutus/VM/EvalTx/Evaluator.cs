863a59bfb057dac5cdd30874e55f156b2401cc97
Rico Miles Quiblat <120778516+ricomiles@users.noreply.github.com>
2025-03-14 03:27:24 +0800

    feat: implement dotnet plutus vm evaluation (#143)
    * feat: plutusvm dotnet initial implementation
    
    * refactor: refactor project namespaces and file structure
    
    * refactor: rename FfiConverter to Interop for clarity
    
    * chore: modify version and support linux build
    
    * chore: fix formatting
    
    * chore: fix incorrect logs
    
    * chore: update pallas dependency to use git source
    
    * refactor: simplify eval module usage in interop
    
    * refactor: update namespace references in interop and utils
    
    * refactor: rename namespace to Plutus.VM
    
    * refactor: rename EvaluateTransaction to EvaluateTx and update related methods
    
    * refactor: rename EvaluateRaw to EvaluateTxRaw in NativeEvaluator
    
    * refactor: simplify function logic and file structure
    
    * refactor: move project to Chrysalis.Plutus instead of creating a new folder
    
    * refactor: update EvaluateTx method to accept byte arrays and then add an overload function that accepts string
    
    * refactor: remove unnecessary blank line in EvaluateTx method

---
+++ src/Chrysalis.Plutus/VM/EvalTx/Evaluator.cs
@@ -0,0 +1,58 @@
+﻿using System.Runtime.InteropServices;
+using Chrysalis.Plutus.VM.Models;
+using Chrysalis.Plutus.VM.Models.Interop;
+using Chrysalis.Plutus.VM.Models.Enums;
+using Chrysalis.Plutus.VM.Interop;
+
+namespace Chrysalis.Plutus.VM.EvalTx;
+
+
+public static class Evaluator
+{
+
+    public static IReadOnlyList<EvaluationResult> EvaluateTx(string txCborHex, string utxosCborHex)
+    {
+        byte[] txCborBytes = Convert.FromHexString(txCborHex);
+        byte[] utxosCborBytes = Convert.FromHexString(utxosCborHex);
+
+        return EvaluateTx(txCborBytes, utxosCborBytes);
+    }
+
+    public static IReadOnlyList<EvaluationResult> EvaluateTx(byte[] txCborBytes, byte[] utxosCborBytes)
+    {
+        TxEvalResultArray resultArray = NativeMethods.EvalTxRaw(
+            txCborBytes,
+            (nuint)txCborBytes.Length,
+            utxosCborBytes,
+            (nuint)utxosCborBytes.Length
+        );
+
+        try
+        {
+
+            var results = new List<EvaluationResult>((int)resultArray.Length);
+            int structSize = Marshal.SizeOf<TxEvalResult>();
+
+            for (int i = 0; i < (int)resultArray.Length; i++)
+            {
+                IntPtr currentPtr = IntPtr.Add(resultArray.Ptr, i * structSize);
+                TxEvalResult result = Marshal.PtrToStructure<TxEvalResult>(currentPtr);
+                results.Add(new EvaluationResult(
+                    (RedeemerTag)result.Tag,
+                    result.Index,
+                    new ExUnits(result.Memory, result.Steps)
+                ));
+            }
+
+            return results;
+        }
+        finally
+        {
+            NativeMethods.FreeEvalResults(resultArray);
+        }
+
+
+    }
+
+
+}
---
+++ src/Chrysalis.Plutus/VM/Models/ExUnits.cs
@@ -0,0 +1,3 @@
+namespace Chrysalis.Plutus.VM.Models;
+
+public record ExUnits(ulong Mem, ulong Steps);
