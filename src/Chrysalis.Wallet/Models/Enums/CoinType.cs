7dc7b25ab0b26c1342e01493e27395df26e04e99
WendellMorTamayo <tamayowendell05@gmail.com>
2025-03-27 02:37:45 +0800

    feat: add enums for Address, Coin, Credential, Derivation, Network, Purpose, and Role types

---
+++ src/Chrysalis.Wallet/Models/Enums/CoinType.cs
@@ -0,0 +1,9 @@
+namespace Chrysalis.Wallet.Models.Enums;
+
+public enum CoinType
+{
+    /// <summary>
+    /// Represents the 'Ada' coin type. 1815 was chosen as it is the year of birth of Ada Lovelace
+    /// </summary>
+    Ada = 1815
+}
