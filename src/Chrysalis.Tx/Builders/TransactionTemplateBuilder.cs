aa17790acc0683b337a04613e1e8f19a0a92df81
Christian Gantuangco <91376403+christiangantuangco@users.noreply.github.com>
2025-04-08 11:44:58 +0800

    feat(tx): set valid from tx building implementation (#184)
    * feat(tx): set valid from tx building implementation
    
    * chore(tx): remove default initialization for ulong validFrom
    
    * feat(tx): setValidTo() tx building implementation

--- src/Chrysalis.Tx/Builders/TransactionTemplateBuilder.cs
+++ src/Chrysalis.Tx/Builders/TransactionTemplateBuilder.cs
@@ -25,7 +25,9 @@ public class TransactionTemplateBuilder<T>
     private readonly List<Action<MintOptions<T>, T>> _mintConfigs = [];
     private readonly List<Action<WithdrawalOptions<T>, T>> _withdrawalConfigs = [];
     private readonly List<string> requiredSigners = [];
-
+    private ulong _validFrom;
+    private ulong _validTo;
+    
     public static TransactionTemplateBuilder<T> Create(ICardanoDataProvider provider) => new TransactionTemplateBuilder<T>().SetProvider(provider);
 
     private TransactionTemplateBuilder<T> SetProvider(ICardanoDataProvider provider)
@@ -61,6 +63,18 @@ public class TransactionTemplateBuilder<T>
     public TransactionTemplateBuilder<T> AddRequiredSigner(string signer)
     {
         requiredSigners.Add(signer);
+        return this;
+    }
+
+    public TransactionTemplateBuilder<T> SetValidFrom(ulong slot)
+    {
+        _validFrom = slot;
+        return this;
+    }
+
+    public TransactionTemplateBuilder<T> SetValidTo(ulong slot)
+    {
+        _validTo = slot;
         return this;
     }
 
@@ -219,6 +233,12 @@ public class TransactionTemplateBuilder<T>
                     context.TxBuilder.AddRequiredSigner(address.GetPaymentKeyHash()!);
                 }
             }
+
+            if (_validFrom > 0)
+                context.TxBuilder.SetValidityIntervalStart(_validFrom);
+            
+            if (_validTo > 0)
+                context.TxBuilder.SetTtl(_validTo);
 
             return context.TxBuilder.CalculateFee(script).Build();
         };
